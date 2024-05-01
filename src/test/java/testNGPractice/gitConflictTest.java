7d918b51f74a227d099696cdb693603dea5f9e72
ErzhanSou <souverigh@gmail.com>
2024-05-01 11:25:30 -0500

    automate something

---
+++ src/test/java/testNGPractice/gitConflictTest.java
@@ -0,0 +1,17 @@
+package testNGPractice;
+
+import org.testng.annotations.Test;
+
+public class gitConflictTest {
+    @Test
+    public void test1(){
+        System.out.println("To get git conflict");
+    }
+
+    @Test
+    public void test2(){
+        int a = 5;
+        int b = 10;
+        System.out.println("Sum: " + (a + b));
+    }
+}
